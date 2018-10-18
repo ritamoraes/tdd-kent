f9e643f872d258efadce72512c68828bd6880889
Rita Moraes <ritac.moraes@gmail.com>
2018-10-18 10:41:16 -0300

    novo commit

--- src/README.md
+++ src/README.md
@@ -5,23 +5,27 @@ TODO List
 4. Rode todos os testes e veja todos funcionarem
 5. Refatore e elimine as duplicidades
 
-Monney Example
 
-$5 + 10 CHF = 10$ if rate is 2:1
-#$5 * 2 = 10$ 
-#Tornar "amount" privado
-#Dollar side-effects
-Arredondamento de Money
-#Equals()
-hashcode
-Equal null
-Equal object
-#5 CHF * 2 = 10 CHF
-Dollar/Franc Duplication
-#Common Equals()
-Common times
-#Compare Francs with Dollars
-Currency
-Delete testFrancMultiplication?
 
-########NEXT CHAPTER 9##########################
+---
+**Monney Example**
+- [ ] $5 + 10 CHF = $10 if rate is 2:1
+- [ ] $5 + $5 = $10
+- [x] $5 * 2 = $10
+- [x] Tornar "amount" privado
+- [x] Dollar side-effects~~
+- [ ] Arredondamento de Money
+- [x] Equals()
+- [ ] hashcode
+- [ ] Equal null
+- [ ] Equal object
+- [x] 5 CHF * 2 = 10 CHF
+- [X] Dollar/Franc Duplication
+- [x] Common Equals()
+- [X] Common Times
+- [x] Compare Francs with Dollars
+- [x] Currency
+- [ ] Delete testFrancMultiplication?
+
+---
+##Proximo Capítulo 12
--- src/main/java/currency/Money.java
+++ src/main/java/currency/Money.java
@@ -1,23 +1,42 @@
 package currency;
 
-public abstract class Money {
+public class Money {
 
     protected int amount;
+    protected String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
 
     public static Money franc(int amount) {
-        return new Franc(amount);
+        return new Money(amount, "CHF");
     }
 
-    public abstract Money times(int multiplier);
-
-    public static Dollar dollar(int amount) {
-        return new Dollar(amount);
+    public static Money dollar(int amount) {
+        return new Money(amount, "USD");
     }
+
+   public  Money times(int multiplier){
+       return new Money(amount * multiplier,currency);
+   }
+
 
 
     public boolean equals(Object o) {
         Money money = (Money) o;
         return amount == money.amount &&
-                getClass().equals(money.getClass());
+                currency().equals(money.currency());
+    }
+
+    public String currency(){
+        return currency;
+    }
+
+    @Override
+    public String toString() {
+        return "amount=" + amount +
+                ", currency='" + currency;
     }
 }
